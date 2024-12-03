a5f7b2d0a45159bbc4f69e74bf270cc384e154db
yann <yannallo@outlook.com>
2024-12-03 14:25:05 +0100

    vm update

--- srcs/requirements/mariadb/conf/sql_init.sh
+++ srcs/requirements/mariadb/conf/sql_init.sh
@@ -1,10 +1,13 @@
-!/bin/bash
+#!/bin/bash
 
 service mysql start
+
 mysql -e "CREATE DATABASE IF NOT EXITS \`${SQL_DATABASE}`\"
 mysql -e "CREATE USER IF NOT EXITS \`${SQL_USER}\`@`'localhost' IDENTIFIED BY `${SQL_PASSWORD}`;"
 mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`{SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASWWORD}';"
 mysql -e "FLUSH PRIVILEGESl;"
+
 mysqladmin -u root -p$SQL_ROOT_PASWWORD shutdown
-exec mysql_safe
+
+exec mysqld_safe
